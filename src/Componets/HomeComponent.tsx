4d2a93b634183a5cb6ef134e07ea9f12d3ae0d76
tiagoBatschke <83837600+tiagoBatschke@users.noreply.github.com>
2023-11-06 05:05:34 -0300

    att

--- src/Componets/HomeComponent.tsx
+++ src/Componets/HomeComponent.tsx
@@ -13,12 +13,12 @@ const HomeComponent:FunctionComponent<HomeComponentProps> =()=>{
 
     return ( 
       <>
-      <div className=' bg-[#FFF0D4] h-[85vh] lg:h-[60vh] w-[100%]  flex flex-col justify-evenly p-1'>
-             <p className='text-[2rem] h-[65vh] lg:h-[40vh] text-center font-bold text-[#DAACAC]'>Logo</p> 
-             <div className='w-[80%] ml-[10%] flex justify-evenly'>
-               <button className='w-[20%] lg:text-[2rem] text-[1.2rem] text-center font-bold text-[#DAACAC]'>O chef</button>
-               <button className='w-[20%] lg:text-[2rem] text-[1.2rem] text-center font-bold text-[#DAACAC]'>Trabalho</button>
-               <button className='w-[20%] lg:text-[2rem] text-[1.2rem] text-center font-bold text-[#DAACAC]'>Orçamento</button>
+      <div className=' bg-[#FFF0D4] h-[85vh] lg:h-[60vh] w-[100%]  flex flex-col items-center justify-evenly p-1'>
+             <img src="./src/assets/Group9.png" className='text-[2rem] lg:h-[30rem] lg:w-[30rem] text-center font-bold text-[#DAACAC]'/> 
+             <div className='w-[90%]  lg:w-[70%]  flex justify-evenly'>
+               <button className='w-[30%] lg:w-[20%] lg:text-[2rem] text-[1.2rem] lg:text-center text-start font-bold text-[#DAACAC]'>O chef</button>
+               <button className='w-[30%] lg:w-[20%] lg:text-[2rem] text-[1.2rem] lg:text-center text-start font-bold text-[#DAACAC]'>Trabalho</button>
+               <button className='w-[30%] lg:w-[20%] lg:text-[2rem] text-[1.2rem] lg:text-center text-start font-bold text-[#DAACAC]'>Orçamento</button>
              </div>
       </div>
       <div className='bg-[#A5554D]  h-[1vh]'>
--- src/Componets/TextImageComponent.tsx
+++ src/Componets/TextImageComponent.tsx
@@ -3,21 +3,21 @@ import {FunctionComponent} from 'react'
 const TextImageComponent:FunctionComponent =()=>{
     return ( 
     <>
-    <div className='h-[10vh] bg-[#A5554D]  flex items-center justify-center'></div>
-    <div className='bg-[#F0CD8C] h-[220vh] p-2 flex flex-col lg:flex-row items-center justify-evenly lg:h-[90vh]'>
+    <div className='h-[3vh] bg-[#A5554D]  flex items-center justify-center'></div>
+    <div className='bg-[#FFF0D4] h-[220vh] p-2 flex flex-col lg:flex-row items-center justify-evenly lg:h-[90vh]'>
       <div className='lg:w-[50%] w-[90%] lg:h-[80vh] flex flex-col items-center justify-evenly'>
         <p className='text-[2rem] flex items-center justify-center text-center font-bold text-[#DAACAC]'>O chef</p>
         <p className='w-[100%] mt-[5%] lg:mt-0 text-center lg:text-justify font-semibold text-[#640A14] lg:w-[80%]'>Meu objetivo é entregar tudo que minha criatividade gastronômica cria edesenvolve. Sou Baiano criado dentro da cozinha, sendo sempre incentivado pelos meus pais e criando essa essência e paixão pela gastronomia. Por esta razão sou chefe de cozinha particular desde 2016, amando minha profissão, tendo a criatividade e segurança com os alimentos para melhor servir aos meus clientes. A  arte da culinária é um extasê a ser vivido, por isso tenho em mente sempre trazer essa experiência para servir em sua mesa.</p>
         <p className='w-[100%] mt-[5%] lg:mt-0 text-center lg:text-justify font-semibold text-[#640A14] lg:w-[80%]'>Minha carreira me proporcionou a confiança e a habilidade para trabalhar com alimentos, garantindo uma experiência culinária excepcional para meus clientes. A arte da culinária é uma paixão que transcende o simples ato de cozinhar; é uma experiência que merece ser vivida em sua plenitude.</p>
         <p className='w-[100%] mt-[5%] lg:mt-0 text-center lg:text-justify font-semibold text-[#640A14] lg:w-[80%]'>Assim, tenho o compromisso constante de trazer essa experiência culinária para a sua mesa. Cada prato que preparo é uma manifestação da minha paixão e criatividade, elaborado com ingredientes de qualidade e uma pitada de amor. Estou pronto para transformar cada refeição em um momento de deleite e satisfação para todos aqueles que tenham o privilégio de saborear minhas criações gastronômicas.</p>
       </div>
-      <img className='lg:w-[30rem] lg:h-[30rem] rounded-md' src="./src/assets/339997666_1001780797870060_8192517577258071711_n.jpg" alt="" />
+      <img className='w-[80%]  lg:w-[30rem] lg:h-[30rem] rounded-3xl' src="./src/assets/339997666_1001780797870060_8192517577258071711_n.jpg" alt="" />
           
     </div>
-    <div className='bg-[#f0cd8c72] p-2 flex justify-evenly items-center'>
-      <div className=' w-[3rem] h-[3rem] bg-[#640A14]'></div>
-      <div className=' w-[3rem] h-[3rem] bg-[#640A14]'></div>
-      <div className=' w-[3rem] h-[3rem] bg-[#640A14]'></div>
+    <div className='bg-[#A55D56] h-[6rem] p-2 flex justify-evenly items-center'>
+      <div><img className='w-[3rem] h-[4rem] ' src="./src/assets/instaImg.svg" alt="" /></div>
+      <div><img className='w-[3rem] h-[3rem] mt-[1rem]' src="./src/assets/email.svg" alt="" /></div>
+      <div><img className='w-[3rem] h-[3rem] mt-[1rem]' src="./src/assets/whatsapp.svg" alt="" /></div>
     </div>
     </>
     )
