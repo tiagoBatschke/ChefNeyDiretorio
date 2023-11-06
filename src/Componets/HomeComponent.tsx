7b87e73f189eb27f0b858ece191793185d46345f
tiagoBatschke <83837600+tiagoBatschke@users.noreply.github.com>
2023-11-06 06:30:56 -0300

    att

--- src/Componets/HomeComponent.tsx
+++ src/Componets/HomeComponent.tsx
@@ -13,16 +13,19 @@ const HomeComponent:FunctionComponent<HomeComponentProps> =()=>{
 
     return ( 
       <>
-      <div className=' bg-[#FFF0D4] h-[85vh] lg:h-[60vh] w-[100%]  flex flex-col items-center justify-evenly p-1'>
-             <img src="./src/assets/Group9.png" className='text-[2rem] lg:h-[30rem] lg:w-[30rem] text-center font-bold text-[#DAACAC]'/> 
-             <div className='w-[90%]  lg:w-[70%]  flex justify-evenly'>
-               <button className='w-[30%] lg:w-[20%] lg:text-[2rem] text-[1.2rem] lg:text-center text-start font-bold text-[#DAACAC]'>O chef</button>
-               <button className='w-[30%] lg:w-[20%] lg:text-[2rem] text-[1.2rem] lg:text-center text-start font-bold text-[#DAACAC]'>Trabalho</button>
-               <button className='w-[30%] lg:w-[20%] lg:text-[2rem] text-[1.2rem] lg:text-center text-start font-bold text-[#DAACAC]'>Orçamento</button>
-             </div>
-      </div>
-      <div className='bg-[#A5554D]  h-[1vh]'>
-        <div className='bg-[#f0cd8cea] rounded-b-3xl  h-[1vh]'></div>
+      <div className='bg-[#FFF0D4]'>
+        <div className='bg-[#FFF0D4] h-[65vh] lg:h-[65vh] w-[100%] lg:w-[60%] lg:ml-[20%] flex flex-col lg:flex-row items-center justify-evenly p-1'>
+          <img src="./src/assets/Group9.png" className='text-[2rem] lg:h-[50rem] lg:w-[50rem] text-center font-bold'/>
+          <div className='w-[100%] lg:w-[40%]'>
+              <div className='lg:w-[100%] w-[101%] lg:ml-0 -ml-[0.5%] bg-[#F0CD8C] lg:h-[30vh] h-[10vh] -mt-[10vh] lg:mt-0 lg:rounded-2xl rounded-b-3xl flex flex-col items-center justify-evenly'>
+                  <div className='w-[90%]  lg:w-[70%] h-[100%]  flex lg:flex-col items-center justify-evenly'>
+                    <button className='w-[32%] bg-[#A5554D]  rounded-3xl shadow-md shadow-[#65322d] lg:w-[60%] lg:h-[5vh] flex flex-row justify-center items-center p-2'><img className='lg:w-[40%]' src="./src/assets/Chef.png" alt="" /></button>
+                    <button className='w-[32%] bg-[#A5554D]  rounded-3xl shadow-md shadow-[#65322d] lg:w-[60%] lg:h-[5vh] flex flex-row justify-center items-center p-2'><img className='lg:w-[40%]' src="./src/assets/Orçamento.png" alt="" /></button>
+                    <button className='w-[32%] bg-[#A5554D]  rounded-3xl shadow-md shadow-[#65322d] lg:w-[60%] lg:h-[5vh] flex flex-row justify-center items-center p-2'><img className='lg:w-[40%]' src="./src/assets/Trabalhos.png" alt="" /></button>
+                  </div>
+              </div>
+          </div> 
+        </div>
       </div>
       </>
     )
