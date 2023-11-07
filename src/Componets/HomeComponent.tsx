c67e24e1b1dfd6093b77b4b34bee524b90e712c4
tiagoBatschke <83837600+tiagoBatschke@users.noreply.github.com>
2023-11-06 23:38:07 -0300

    att

--- src/Componets/HomeComponent.tsx
+++ src/Componets/HomeComponent.tsx
@@ -14,7 +14,7 @@ const HomeComponent:FunctionComponent<HomeComponentProps> =()=>{
     return ( 
       <>
       <div className='bg-[#FFF0D4]'>
-        <div className='bg-[#FFF0D4] h-[65vh] lg:h-[65vh] w-[100%] lg:w-[60%] lg:ml-[20%] flex flex-col lg:flex-row items-center justify-evenly p-1'>
+        <div className='bg-[#FFF0D4] h-[65vh] lg:h-[65vh] w-[100%] lg:w-[70%] lg:ml-[15%] flex flex-col lg:flex-row items-center justify-evenly p-1'>
           <img src="./src/assets/Group9.png" className='text-[2rem] lg:h-[50rem] lg:w-[50rem] text-center font-bold'/>
           <div className='w-[100%] lg:w-[40%]'>
               <div className='lg:w-[100%] w-[101%] lg:ml-0 -ml-[0.5%] bg-[#F0CD8C] lg:h-[30vh] h-[10vh] -mt-[10vh] lg:mt-0 lg:rounded-2xl rounded-b-3xl flex flex-col items-center justify-evenly'>
