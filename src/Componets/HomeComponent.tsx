7c3284ca201020f5ed63818c6395e08e0256dcf4
tiagoBatschke <83837600+tiagoBatschke@users.noreply.github.com>
2023-11-12 06:05:11 -0300

    att

--- src/Componets/HomeComponent.tsx
+++ src/Componets/HomeComponent.tsx
@@ -19,9 +19,9 @@ const HomeComponent:FunctionComponent<HomeComponentProps> =()=>{
           <div className='w-[100%] lg:w-[40%]'>
               <div className='lg:w-[100%] w-[101%] lg:ml-0 -ml-[0.5%] bg-[#F0CD8C] lg:h-[30vh] h-[10vh] -mt-[10vh] lg:mt-0 lg:rounded-2xl rounded-b-3xl flex flex-col items-center justify-evenly'>
                   <div className='w-[90%]  lg:w-[70%] h-[100%]  flex lg:flex-col items-center justify-evenly'>
-                    <button className='w-[32%] bg-[#A5554D]  rounded-3xl shadow-md shadow-[#65322d] lg:w-[60%] lg:h-[5vh] flex flex-row justify-center items-center p-2'><img className='lg:w-[40%]' src="./src/assets/Chef.png" alt="" /></button>
-                    <button className='w-[32%] bg-[#A5554D]  rounded-3xl shadow-md shadow-[#65322d] lg:w-[60%] lg:h-[5vh] flex flex-row justify-center items-center p-2'><img className='lg:w-[40%]' src="./src/assets/Orçamento.png" alt="" /></button>
-                    <button className='w-[32%] bg-[#A5554D]  rounded-3xl shadow-md shadow-[#65322d] lg:w-[60%] lg:h-[5vh] flex flex-row justify-center items-center p-2'><img className='lg:w-[40%]' src="./src/assets/Trabalhos.png" alt="" /></button>
+                    <button className='w-[32%] bg-[#A5554D]  rounded-3xl shadow-md shadow-[#65322d] lg:w-[60%] lg:h-[5vh] flex flex-row justify-center items-center p-2'><img className='lg:w-[40%]' src="/src/assets/Chef.png" alt="" /></button>
+                    <button className='w-[32%] bg-[#A5554D]  rounded-3xl shadow-md shadow-[#65322d] lg:w-[60%] lg:h-[5vh] flex flex-row justify-center items-center p-2'><img className='lg:w-[40%]' src="/src/assets/Orçamento.png" alt="" /></button>
+                    <button className='w-[32%] bg-[#A5554D]  rounded-3xl shadow-md shadow-[#65322d] lg:w-[60%] lg:h-[5vh] flex flex-row justify-center items-center p-2'><img className='lg:w-[40%]' src="/src/assets/Trabalhos.png" alt="" /></button>
                   </div>
               </div>
           </div> 
--- src/Componets/TextImageComponent.tsx
+++ src/Componets/TextImageComponent.tsx
@@ -10,7 +10,7 @@ const TextImageComponent:FunctionComponent =()=>{
         <p className='w-[100%] mt-[5%] lg:mt-0 text-center lg:text-justify lg:text-[1rem] font-semibold text-[#640A14] lg:w-[80%]'>Minha carreira me proporcionou a confiança e a habilidade para trabalhar com alimentos, garantindo uma experiência culinária excepcional para meus clientes. A arte da culinária é uma paixão que transcende o simples ato de cozinhar; é uma experiência que merece ser vivida em sua plenitude.</p>
         <p className='w-[100%] mt-[5%] lg:mt-0 text-center lg:text-justify lg:text-[1rem] font-semibold text-[#640A14] lg:w-[80%]'>Assim, tenho o compromisso constante de trazer essa experiência culinária para a sua mesa. Cada prato que preparo é uma manifestação da minha paixão e criatividade, elaborado com ingredientes de qualidade e uma pitada de amor. Estou pronto para transformar cada refeição em um momento de deleite e satisfação para todos aqueles que tenham o privilégio de saborear minhas criações gastronômicas.</p>
       </div>
-      <img className='w-[80%] lg:w-[30rem] lg:h-[30rem] rounded-3xl' src="./src/assets/339997666_1001780797870060_8192517577258071711_n.jpg" alt="" /> 
+      <img className='w-[80%] lg:w-[30rem] lg:h-[30rem] rounded-3xl' src="/src/assets/339997666_1001780797870060_8192517577258071711_n.jpg" alt="" /> 
     </div>
 
     {/* <div className='lg:bg-gradient-to-l bg-gradient-to-t from-[#E98A54]  to-[#e2cab358] h-[220vh] flex flex-col lg:flex-row-reverse items-center justify-evenly lg:h-[90vh]'>
@@ -25,9 +25,9 @@ const TextImageComponent:FunctionComponent =()=>{
           
     </div> */}
     <div className='bg-[#A55D56] h-[6rem] p-2 flex justify-evenly items-center'>
-      <div><img className='w-[3rem] h-[4rem]' src="./src/assets/instaImg.svg" alt="" /></div>
-      <div><img className='w-[3rem] h-[3rem]mt-[1rem]' src="./src/assets/email.svg" alt="" /></div>
-      <div><img className='w-[3rem] h-[3rem]mt-[1rem]' src="./src/assets/whatsapp.svg" alt="" /></div>
+      <div><img className='w-[3rem] h-[4rem]' src="/src/assets/instaImg.svg" alt="" /></div>
+      <div><img className='w-[3rem] h-[3rem]mt-[1rem]' src="/src/assets/email.svg" alt="" /></div>
+      <div><img className='w-[3rem] h-[3rem]mt-[1rem]' src="/src/assets/whatsapp.svg" alt="" /></div>
     </div>
     </>
     )
